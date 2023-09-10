2ca1149efafff68ffb2bab78c4c22d92118f3140
Donghyun Shin <apersoaltps@gmail.com>
2023-09-10 18:11:42 +0700

    qwer

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ use anyhow::{anyhow, Ok, Result};
 use nix::sched::{setns, CloneFlags};
 use std::{
     future::Future,
-    os::fd::AsRawFd,
     process,
     time::{Duration, Instant},
 };
@@ -13,8 +12,7 @@ use tokio::{
     process::Command,
     signal::ctrl_c,
     spawn,
-    task::JoinHandle,
-    time::sleep,
+    task::{JoinHandle, JoinSet},
 };
 
 fn spawn_task(future: impl Future<Output = Result<()>> + Send + 'static) -> JoinHandle<()> {
@@ -46,54 +44,105 @@ async fn exit() -> Result<()> {
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    // try to delete network namespace
     let _ = exec("ip netns delete server").await;
     let _ = exec("ip netns delete client").await;
 
+    // handle sigint
     spawn_task(async {
         ctrl_c().await?;
         exit().await
     });
 
+    // create network namespace
     exec("ip netns add server").await?;
     exec("ip netns add client").await?;
 
-    let server = File::open("/var/run/netns/server").await?;
-    let client = File::open("/var/run/netns/client").await?;
-
+    // interface setup
     exec("ip link add dev server netns server type veth peer name client netns client").await?;
     exec("ip netns exec server ip link set dev server up").await?;
     exec("ip netns exec client ip link set dev client up").await?;
     exec("ip netns exec server ip addr add dev server 10.1.1.1/24").await?;
     exec("ip netns exec client ip addr add dev client 10.1.1.2/24").await?;
 
-    exec("ip netns exec server tc qdisc add dev server root netem delay 10ms").await?;
-    exec("ip netns exec client tc qdisc add dev client root netem delay 10ms").await?;
-    // exec("ip netns exec client tc qdisc add dev client root netem loss 10%@").await?;
+    // run the measurements
+    for (impairment, quantities) in [
+        (
+            "delay",
+            (0..5).map(|e| format!("{}ms", e * 20)).collect::<Vec<_>>(),
+        ),
+        (
+            "loss",
+            (0..5).map(|e| format!("{}%", e)).collect::<Vec<_>>(),
+        ),
+    ] {
+        for quantity in quantities {
+            println!("Measuring {} {}", impairment, quantity);
 
-    setns(server.as_raw_fd(), CloneFlags::empty())?;
-    let lstn = TcpListener::bind("10.1.1.1:1234").await?;
-    spawn_task(async move {
-        let mut strm = lstn.accept().await?.0;
-        loop {
-            let data = strm.read_u128().await?;
-            strm.write_u128(data).await?;
+            // create conditions
+            exec(format!(
+                "ip netns exec client tc qdisc add dev client root netem {} {}",
+                impairment, quantity
+            ))
+            .await?;
+            exec(format!(
+                "ip netns exec server tc qdisc add dev server root netem {} {}",
+                impairment, quantity
+            ))
+            .await?;
+
+            // create server socket
+            let server = File::open("/var/run/netns/server").await?;
+            setns(server, CloneFlags::empty())?;
+            let server = TcpListener::bind("10.1.1.1:1234").await?;
+
+            // create client socket
+            let client = File::open("/var/run/netns/client").await?;
+            setns(client, CloneFlags::empty())?;
+            let client = TcpSocket::new_v4()?;
+            client.bind("10.1.1.2:0".parse()?)?;
+
+            let mut join_set = JoinSet::new();
+
+            // spawn server
+            join_set.spawn(spawn_task(async move {
+                let mut stream = server.accept().await?.0;
+                let mut buffer = [0; 1024];
+                let mut amount = 0;
+                let now = Instant::now();
+                while now.elapsed() < Duration::from_secs(1) {
+                    amount = amount + stream.read(&mut buffer).await?;
+                }
+                println!("{}Mb/s", amount * 8 / 1024 / 1024 / 1);
+                Ok(())
+            }));
+
+            // spawn client
+            join_set.spawn(spawn_task(async {
+                let mut stream = client.connect("10.1.1.1:1234".parse()?).await?;
+                let now = Instant::now();
+                while now.elapsed() < Duration::from_secs(1) {
+                    stream.write(&[0; 1024]).await?;
+                }
+                Ok(())
+            }));
+
+            // await both futures
+            while join_set.join_next().await.is_some() {}
+
+            // delete conditions
+            exec(format!(
+                "ip netns exec client tc qdisc del dev client root netem {} {}",
+                impairment, quantity
+            ))
+            .await?;
+            exec(format!(
+                "ip netns exec server tc qdisc del dev server root netem {} {}",
+                impairment, quantity
+            ))
+            .await?;
         }
-    });
-
-    setns(client.as_raw_fd(), CloneFlags::empty())?;
-    let sock = TcpSocket::new_v4()?;
-    sock.bind("10.1.1.2:1234".parse()?)?;
-    let mut strm = sock.connect("10.1.1.1:1234".parse()?).await?;
-    spawn_task(async move {
-        loop {
-            let inst = Instant::now();
-            strm.write_u128(inst.elapsed().as_millis()).await?;
-            let data = strm.read_u128().await?;
-            println!("{}", inst.elapsed().as_millis() - data);
-        }
-    });
-
-    sleep(Duration::MAX).await;
+    }
 
     exit().await
 }
