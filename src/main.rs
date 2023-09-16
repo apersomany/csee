8e24d4b74933fad8c84e2a800d37ebceffe0cf2c
Donghyun Shin <apersoaltps@gmail.com>
2023-09-16 13:01:44 +0000

    update

--- src/main.rs
+++ src/main.rs
@@ -1,148 +1,133 @@
-use anyhow::{anyhow, Ok, Result};
-use nix::sched::{setns, CloneFlags};
 use std::{
-    future::Future,
-    process,
-    time::{Duration, Instant},
-};
-use tokio::{
     fs::File,
-    io::{AsyncReadExt, AsyncWriteExt},
-    net::{TcpListener, TcpSocket},
+    io::{Read, Write},
+    net::{TcpListener, TcpStream},
+    os::fd::AsRawFd,
     process::Command,
-    signal::ctrl_c,
-    spawn,
-    task::{JoinHandle, JoinSet},
+    sync::{
+        atomic::{AtomicUsize, Ordering},
+        Arc,
+    },
+    thread::spawn,
+    time::Duration,
 };
 
-fn spawn_task(future: impl Future<Output = Result<()>> + Send + 'static) -> JoinHandle<()> {
-    spawn(async move {
-        if let Err(e) = future.await {
-            println!("{}", e)
-        }
-    })
-}
+use anyhow::{Error, Result};
+use plotters::prelude::*;
 
-async fn exec(command: impl AsRef<str>) -> Result<()> {
+fn execute(command: impl AsRef<str>, netns: Option<&str>) -> Result<()> {
     let mut command = command.as_ref().split_whitespace();
-    let output = Command::new(command.next().unwrap())
-        .args(command)
-        .output()
-        .await?;
+    let output = if let Some(netns) = netns {
+        Command::new("ip")
+            .args(["netns", "exec", netns])
+            .args(command)
+            .output()?
+    } else {
+        Command::new(command.next().unwrap())
+            .args(command)
+            .output()?
+    };
     if output.status.success() {
         Ok(())
     } else {
-        Err(anyhow!("{}", String::from_utf8(output.stderr)?))
+        Err(Error::msg(String::from_utf8(output.stderr)?))
     }
 }
 
-async fn exit() -> Result<()> {
-    exec("ip netns delete server").await?;
-    exec("ip netns delete client").await?;
-    process::exit(0);
-}
-
-#[tokio::main]
-async fn main() -> Result<()> {
-    // try to delete network namespace
-    let _ = exec("ip netns delete server").await;
-    let _ = exec("ip netns delete client").await;
-
-    // handle sigint
-    spawn_task(async {
-        ctrl_c().await?;
-        exit().await
-    });
-
-    // create network namespace
-    exec("ip netns add server").await?;
-    exec("ip netns add client").await?;
-
-    // interface setup
-    exec("ip link add dev server netns server type veth peer name client netns client").await?;
-    exec("ip netns exec server ip link set dev server up").await?;
-    exec("ip netns exec client ip link set dev client up").await?;
-    exec("ip netns exec server ip addr add dev server 10.1.1.1/24").await?;
-    exec("ip netns exec client ip addr add dev client 10.1.1.2/24").await?;
-
-    // run the measurements
-    for (impairment, quantities) in [
-        (
-            "delay",
-            (0..5).map(|e| format!("{}ms", e * 20)).collect::<Vec<_>>(),
-        ),
-        (
-            "loss",
-            (0..5).map(|e| format!("{}%", e)).collect::<Vec<_>>(),
-        ),
-    ] {
-        for quantity in quantities {
-            println!("Measuring {} {}", impairment, quantity);
-
-            // create conditions
-            exec(format!(
-                "ip netns exec client tc qdisc add dev client root netem {} {}",
-                impairment, quantity
-            ))
-            .await?;
-            exec(format!(
-                "ip netns exec server tc qdisc add dev server root netem {} {}",
-                impairment, quantity
-            ))
-            .await?;
-
-            // create server socket
-            let server = File::open("/var/run/netns/server").await?;
-            setns(server, CloneFlags::empty())?;
-            let server = TcpListener::bind("10.1.1.1:1234").await?;
-
-            // create client socket
-            let client = File::open("/var/run/netns/client").await?;
-            setns(client, CloneFlags::empty())?;
-            let client = TcpSocket::new_v4()?;
-            client.bind("10.1.1.2:0".parse()?)?;
-
-            let mut join_set = JoinSet::new();
-
-            // spawn server
-            join_set.spawn(spawn_task(async move {
-                let mut stream = server.accept().await?.0;
-                let mut buffer = [0; 1024];
-                let mut amount = 0;
-                let now = Instant::now();
-                while now.elapsed() < Duration::from_secs(1) {
-                    amount = amount + stream.read(&mut buffer).await?;
-                }
-                println!("{}Mb/s", amount * 8 / 1024 / 1024 / 1);
-                Ok(())
-            }));
-
-            // spawn client
-            join_set.spawn(spawn_task(async {
-                let mut stream = client.connect("10.1.1.1:1234".parse()?).await?;
-                let now = Instant::now();
-                while now.elapsed() < Duration::from_secs(1) {
-                    stream.write(&[0; 1024]).await?;
-                }
-                Ok(())
-            }));
-
-            // await both futures
-            while join_set.join_next().await.is_some() {}
-
-            // delete conditions
-            exec(format!(
-                "ip netns exec client tc qdisc del dev client root netem {} {}",
-                impairment, quantity
-            ))
-            .await?;
-            exec(format!(
-                "ip netns exec server tc qdisc del dev server root netem {} {}",
-                impairment, quantity
-            ))
-            .await?;
+fn set_netns(netns: &str) -> Result<()> {
+    unsafe {
+        if libc::setns(
+            File::open(format!("/var/run/netns/{netns}"))?.as_raw_fd(),
+            0,
+        ) < 0
+        {
+            Err(std::io::Error::last_os_error().into())
+        } else {
+            Ok(())
         }
     }
+}
 
-    exit().await
+fn init() -> Result<()> {
+    // create network namespaces
+    for side in ["server", "client"] {
+        execute(format!("ip netns add {side}"), None)?;
+    }
+    // create network interfaces
+    execute(
+        "ip link add dev server netns server type veth peer name client netns client",
+        None,
+    )?;
+    // configure the network interfaces
+    for (side, addr) in [("server", "10.1.1.1/24"), ("client", "10.1.1.2/24")] {
+        execute(format!("ip link set dev {side} up"), Some(side))?;
+        execute(format!("ip addr add dev {side} {addr}"), Some(side))?;
+    }
+    set_netns("server")?;
+    let server = TcpListener::bind("10.1.1.1:1234")?;
+    spawn(move || {
+        while let Ok((mut stream, _)) = server.accept() {
+            while let Ok(_) = stream.write_all([0; 1024].as_ref()) {}
+        }
+    });
+    set_netns("client")?;
+    Ok(())
+}
+
+fn clean() -> Result<()> {
+    // delete network namespaces
+    for side in ["server", "client"] {
+        execute(format!("ip netns delete {side}"), None)?;
+    }
+    Ok(())
+}
+
+fn run_trial(impairment: &str, quantity: &str) -> Result<()> {
+    const DURATION: f64 = 0.05f64;
+    const DIVISION: usize = 200;
+    execute(
+        format!("tc qdisc add dev server root netem {impairment} {quantity} rate 1gbit"),
+        Some("server"),
+    )?;
+    let mut stream = TcpStream::connect("10.1.1.1:1234")?;
+    let mut buffer = Box::new([0; 1024]);
+    let amount = Arc::new(AtomicUsize::new(0));
+    let speeds = {
+        let amount = amount.clone();
+        spawn(move || {
+            let mut speeds = Vec::new();
+            for _ in 0..DIVISION {
+                std::thread::sleep(Duration::from_secs_f64(DURATION));
+                speeds.push(amount.swap(0, Ordering::AcqRel) as f64 / DURATION / 2f64.powf(17f64));
+            }
+            speeds
+        })
+    };
+    while !speeds.is_finished() {
+        amount.fetch_add(stream.read(buffer.as_mut())?, Ordering::AcqRel);
+    }
+    execute(
+        format!("tc qdisc del dev server root netem {impairment} {quantity} rate 1gbit"),
+        Some("server"),
+    )?;
+    let path = format!("{impairment}_{quantity}.png");
+    let root = BitMapBackend::new(&path, (1024, 512)).into_drawing_area();
+    root.fill(&WHITE)?;
+    let mut chart =
+        ChartBuilder::on(&root).build_cartesian_2d(0..DIVISION, 0f64..2f64.powf(10f64))?;
+    chart.draw_series(LineSeries::new(
+        speeds.join().unwrap().into_iter().enumerate(),
+        &RED,
+    ))?;
+    root.present()?;
+    Ok(())
+}
+
+fn main() {
+    let _ = clean();
+    init().unwrap();
+    for i in 0..6 {
+        run_trial("duplicate", &format!("{}%", i * 10)).unwrap();
+    }
+    clean().unwrap();
 }
